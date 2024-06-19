b6fdeec382845a698ab3a8a0bdd5779dd4177db5
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-06-19 23:39:59 +0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Hina Khan on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
