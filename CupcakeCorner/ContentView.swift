c1a833bf9602992923679a3513a2a0aa430fed7d
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-04 23:27:53 +0500

    Sending and receiving Codable data with URLSession and SwiftUI

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -18,6 +18,8 @@ struct Result: Codable {
 
 struct ContentView: View {
     @State private var results = [Result]()
+    
+    
 
     var body: some View {
         List(results, id: \.trackId) { item in
@@ -26,6 +28,28 @@ struct ContentView: View {
                     .font(.headline)
                 Text(item.collectionName)
             }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // more code to come
+        } catch {
+            print("Invalid data")
+        }
+        
+        if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+            results = decodedResponse.results
         }
     }
 }
