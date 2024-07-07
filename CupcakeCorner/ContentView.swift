d46878ac004616a0efa41b37b6767a8fcb10f07b
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-07 23:17:55 +0500

    Validating and disabling forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -6,52 +6,25 @@
 //
 
 import SwiftUI
-struct Response: Codable {
-    var results: [Result]
-}
 
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
 
     var body: some View {
-        VStack{
-            AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } else if phase.error != nil {
-                    Text("There was an error loading the image.")
-                } else {
-                    ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
                 }
             }
-            .frame(width: 200, height: 200)
-//            }
         }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-        
     }
 }
 
