b04136c03887eb2973a0852e2e99f5fb30f601ea
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-14 23:34:10 +0500

    create a new SwiftUI view called CheckoutView

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Hina Khan on 14/07/2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
