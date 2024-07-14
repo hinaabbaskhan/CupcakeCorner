96329c2c4ebadb6ca5561ef0db55a1e9517b0c96
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-14 23:29:47 +0500

    Taking basic order details

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -13,7 +13,15 @@ class Order {
     var type = 0
     var quantity = 3
 
-    var specialRequestEnabled = false
     var extraFrosting = false
     var addSprinkles = false
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
 }
