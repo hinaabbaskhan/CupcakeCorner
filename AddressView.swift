95878d253885173b3e0dd1a02593b8cede8897a4
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-14 23:29:34 +0500

    add address view

---
+++ AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Hina Khan on 14/07/2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
