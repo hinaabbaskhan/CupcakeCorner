f3736d5e403cb12e6b8a40df0c830b842a683e0b
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-17 23:30:21 +0500

    Sending and receiving orders over the internet

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -34,6 +34,25 @@ struct CheckoutView: View {
         .scrollBounceBehavior(.basedOnSize)
 
     }
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
+    }
 }
 
 #Preview {
