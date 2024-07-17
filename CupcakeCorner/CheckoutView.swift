a364bda8ba0ede72a9a0166504a70cf94acf1f3b
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-17 23:05:09 +0500

    create CheckoutView

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @Bindable var order: Order
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
 #Preview {
-    CheckoutView()
+    CheckoutView(order: Order())
 }
