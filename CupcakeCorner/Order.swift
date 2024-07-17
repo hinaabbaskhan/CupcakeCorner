27e3c37bda5b99bceb4405c1a71f2707e081cffd
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-17 23:04:26 +0500

    Check for a valid address

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -13,8 +13,6 @@ class Order {
     var type = 0
     var quantity = 3
 
-    var extraFrosting = false
-    var addSprinkles = false
     
     var specialRequestEnabled = false {
         didSet {
@@ -23,5 +21,20 @@ class Order {
                 addSprinkles = false
             }
         }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+
+        return true
     }
 }
